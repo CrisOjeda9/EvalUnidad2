fd1029255b8d7c6e97c50594ea289d33518ed77d
Cristian <210180@utxicotepec.edu.mx>
2024-06-20 15:17:12 -0600

    Creacion de GET y POST

--- backend/routes/persona.py
+++ backend/routes/persona.py
@@ -1,8 +1,34 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
 
 persona = APIRouter()
+personas = []
+
+class model_personas(BaseModel):
+    id:int
+    nombre:str
+    primer_apellido:str
+    segundo_apellido:Optional[str]
+    edad:int
+    fecha_nacimiento:datetime
+    curp:str
+    tipo_sangre:str
+    created_at:datetime = datetime.now()
+    estatus:bool = False
+
+
+
+@persona.get('/')
+def bienvenida():
+    return "Bienvenido al api del sistema"
+
 @persona.get("/personas")
-def helloworld():
-    return "Hello World"
+def get_personas():
+    return personas
 
-
+@persona.post("/personas")
+def savePersonas(datos_persona:model_personas):
+    personas.append(datos_persona)
+    return "Datos guardados correctamente"
