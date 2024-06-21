d4981024b901022b54d488bcb29cec6cdff26716
Cristian <210180@utxicotepec.edu.mx>
2024-06-21 13:50:32 -0600

    Creacion del modelo Usuario

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from routes.persona import persona
+from routes.usuario import usuario
 
 app= FastAPI()
 app.include_router(persona)
+app.include_router(usuario)
+
 
 print("Bienvenido a mi aplicacion")
