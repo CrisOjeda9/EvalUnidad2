4bd9da103cb662481f2e7b824e7f683da8a23ddb
Cristian <210180@utxicotepec.edu.mx>
2024-07-05 14:28:22 -0600

    Personas

--- backend/app.py
+++ backend/app.py
@@ -2,11 +2,14 @@ from fastapi import FastAPI
 #from routes.persona import persona
 #from routes.usuario import usuario
 from routes.users import user
+from routes.persons import person
 
 app= FastAPI()
 #app.include_router(persona)
 #app.include_router(usuario)
 app.include_router(user)
+app.include_router(person)
+
 
 
 print("Bienvenido a mi aplicacion")
