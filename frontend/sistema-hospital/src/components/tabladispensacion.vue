5af4fe9a0ced056ef94d6b051988436ff0007392
“Cristian <210180@utxicotepec.edu.mx>
2024-07-31 19:36:37 -0600

    Cambios Dispensacion

--- frontend/sistema-hospital/src/components/tabladispensacion.vue
+++ frontend/sistema-hospital/src/components/tabladispensacion.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="min-h-screen bg-gray-200 bg-opacity-100  flex-col justify-center py-5 sm:px-6 lg:px-8 px-6 bg-repeat  ">
+    <div class="min-h-screen bg-gray-200 bg-opacity-100 flex-col justify-center py-5 sm:px-6 lg:px-8 px-6 bg-repeat">
         <div class="sm:mx-auto sm:w-full sm:max-w-md">
             <img class="mx-auto h-10 w-auto" src="https://www.svgrepo.com/show/301692/login.svg" alt="Workflow">
             <h2 class="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
-                Dispensacion
+                Dispensación de Medicamentos - Recetas
             </h2>
         </div>
         <a href="/dispensation"
@@ -11,87 +11,95 @@
             Agregar +
         </a>
 
-
-
-
         <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-6">
             <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
-                        <th scope="col" class="px-6 py-3">
-                            Product name
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Color
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Category
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Price
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            <span class="sr-only">Edit</span>
-                        </th>
+                        <th scope="col" class="px-6 py-3">ID</th>
+                        <th scope="col" class="px-6 py-3">Receta Médica ID</th>
+                        <th scope="col" class="px-6 py-3">Personal Médico ID</th>
+                        <th scope="col" class="px-6 py-3">Solicitud ID</th>
+                        <th scope="col" class="px-6 py-3">Estatus</th>
+                        <th scope="col" class="px-6 py-3">Tipo</th>
+                        <th scope="col" class="px-6 py-3">Total Medicamentos Entregados</th>
+                        <th scope="col" class="px-6 py-3">Total Costo</th>
+                        <th scope="col" class="px-6 py-3">Fecha Registro</th>
+                        <th scope="col" class="px-6 py-3">Fecha Actualización</th>
+                        <th scope="col" class="px-6 py-3"><span class="sr-only">Eliminar</span></th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr
-                        class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Apple MacBook Pro 17"
-                        </th>
-                        <td class="px-6 py-4">
-                            Silver
-                        </td>
-                        <td class="px-6 py-4">
-                            Laptop
-                        </td>
-                        <td class="px-6 py-4">
-                            $2999
-                        </td>
+                    <tr v-for="item in items" :key="item.id" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <td class="px-6 py-4">{{ item.id }}</td>
+                        <td class="px-6 py-4">{{ item.recetaMedicaId || 'No Aplica' }}</td>
+                        <td class="px-6 py-4">{{ item.personalMedicoId || 'No Aplica' }}</td>
+                        <td class="px-6 py-4">{{ item.solicitudId || 'No Aplica' }}</td>
+                        <td class="px-6 py-4">{{ item.estatus }}</td>
+                        <td class="px-6 py-4">{{ item.tipo }}</td>
+                        <td class="px-6 py-4">{{ item.totalMedicamentosEntregados }}</td>
+                        <td class="px-6 py-4">{{ item.totalCosto.toFixed(2) }}</td>
+                        <td class="px-6 py-4">{{ item.fechaRegistro }}</td>
+                        <td class="px-6 py-4">{{ item.fechaActualizacion }}</td>
                         <td class="px-6 py-4 text-right">
-                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                        </td>
-                    </tr>
-                    <tr
-                        class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Microsoft Surface Pro
-                        </th>
-                        <td class="px-6 py-4">
-                            White
-                        </td>
-                        <td class="px-6 py-4">
-                            Laptop PC
-                        </td>
-                        <td class="px-6 py-4">
-                            $1999
-                        </td>
-                        <td class="px-6 py-4 text-right">
-                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                        </td>
-                    </tr>
-                    <tr class="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Magic Mouse 2
-                        </th>
-                        <td class="px-6 py-4">
-                            Black
-                        </td>
-                        <td class="px-6 py-4">
-                            Accessories
-                        </td>
-                        <td class="px-6 py-4">
-                            $99
-                        </td>
-                        <td class="px-6 py-4 text-right">
-                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                            <a href="#" @click.prevent="handleDelete(item.id)" class="font-medium text-red-600 hover:underline">Eliminar</a>
                         </td>
                     </tr>
                 </tbody>
             </table>
         </div>
-
-
-</div></template>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            items: [
+                {
+                    id: 1,
+                    recetaMedicaId: 1001,
+                    personalMedicoId: 2001,
+                    solicitudId: null,
+                    estatus: 'Abastecida',
+                    tipo: 'Pública',
+                    totalMedicamentosEntregados: 10,
+                    totalCosto: 500.00,
+                    fechaRegistro: '2024-07-30 10:00:00',
+                    fechaActualizacion: '2024-07-31 14:00:00',
+                },
+                {
+                    id: 2,
+                    recetaMedicaId: 1002,
+                    personalMedicoId: 2002,
+                    solicitudId: null,
+                    estatus: 'Parcialmente abastecida',
+                    tipo: 'Privada',
+                    totalMedicamentosEntregados: 5,
+                    totalCosto: 250.00,
+                    fechaRegistro: '2024-07-31 11:00:00',
+                    fechaActualizacion: '2024-08-01 15:00:00',
+                },
+                {
+                    id: 3,
+                    recetaMedicaId: 1003,
+                    personalMedicoId: 2003,
+                    solicitudId: null,
+                    estatus: 'Abastecida',
+                    tipo: 'Mixta',
+                    totalMedicamentosEntregados: 8,
+                    totalCosto: 400.00,
+                    fechaRegistro: '2024-08-01 09:00:00',
+                    fechaActualizacion: '2024-08-02 16:00:00',
+                },
+            ]
+        };
+    },
+    methods: {
+        handleDelete(id) {
+            // Aquí puedes hacer una solicitud al servidor para eliminar el registro.
+            // Por ejemplo: axios.delete(`/api/dispensation/${id}`)
+            // Para la simulación, eliminamos el elemento del array local.
+            this.items = this.items.filter(item => item.id !== id);
+        }
+    }
+};
+</script>
